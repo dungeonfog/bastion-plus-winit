b95e3b625dcf14211e7a97d4040c12a2e3a1bc2e
Andreas Monitzer <andreas@monitzer.com>
2020-07-29 01:36:03 +0200

    Trying to move the renderer to its own blocking thread.

--- src/renderer.rs
+++ src/renderer.rs
@@ -10,20 +10,28 @@ pub struct Renderer {}
 
 impl Renderer {
     pub async fn exec(ctx: BastionContext) -> Result<(), ()> {
-        loop {
-            msg! { ctx.recv().await?,
-                msg: RenderMessage => {
-                    log::debug!("render msg: {:?}", msg);
-                };
-                ref msg: super::QuitMessage => {
-                    log::info!("Renderer shutting down.");
-                    break;
-                };
-                _: _ => panic!("Unknown message received");
-            }
-        }
-
-        Ok(())
+        log::debug!("Starting renderer...");
+        let task = blocking! {
+            log::debug!("On blocking thread");
+            run!(async move {
+                log::debug!("Running blocking thread");
+                loop {
+                    log::debug!("Before msg!");
+                    msg! { ctx.recv().await?,
+                        msg: RenderMessage => {
+                            log::debug!("render msg: {:?}", msg);
+                        };
+                        ref msg: super::QuitMessage => {
+                            log::info!("Renderer shutting down.");
+                            break;
+                        };
+                        _: _ => panic!("Unknown message received");
+                    }
+                }
+                Ok(())
+            })
+        };
+        task.await.unwrap()
     }
     // fn new() -> Self {
     //     Self {}
